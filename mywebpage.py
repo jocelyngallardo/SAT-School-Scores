8e0950bf106ac8059b8d6f25ecaffed8f7d2d3fd
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-11-21 08:37:19 -0800

    'updated'

--- mywebpage.py
+++ mywebpage.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def render_main():
     return render_template('home.html')
+    
+@app.route("/page1")
+def render_page1():
+    return render_template('page1.html')
+    
+@app.route("/page2")
+def render_page2():
+    return render_template('page2.html')
+    
+@app.route("/page3")
+def render_page3():
+    return render_template('page3.html')
 
 if __name__=="__main__":
     app.run(debug=False)
