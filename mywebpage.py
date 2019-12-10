1689856c7f4414515a2ce6178d3d664e98ad09df
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-12-10 09:32:29 -0800

    this works

--- mywebpage.py
+++ mywebpage.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def render_main():
     with open('school_scores.json') as sat_data:
         satData = json.load(sat_data)
+        print(get_states)
     return render_template('home.html')
     
 @app.route("/stateDemographics")
@@ -30,15 +31,26 @@ def render_academics_and_GPA():
         dataVerbal = ''
         listOfStates = []
         for states in satData:
-            if states['State']['Name'] not in listOfStates and states['Year'] == 2015:
-                listOfStates.append(states['State']['Name'])
-                print(listOfStates)
-                for grade in listOfStates[0]['GPA']:
-                        dataMath += Markup('{ y: ' + str(listOfStates[0]['GPA'][grade]['Math']) + ', label: ' + '"' + grade + '"' + ' }' + ',')
-                        dataVerbal += Markup('{ y: ' + str(listOfStates[0]['GPA'][grade]['Verbal']) + ', label: ' + '"' + grade + '"' + ' }' + ',')
+            if states['State']['Code'] =='AL'and states['Year'] == 2015:
+                for grade in states['GPA']:
+                        dataMath += Markup('{ y: ' + str(states['GPA'][grade]['Math']) + ', label: ' + '"' + grade + '"' + ' }' + ',')
+                        dataVerbal += Markup('{ y: ' + str(states['GPA'][grade]['Verbal']) + ', label: ' + '"' + grade + '"' + ' }' + ',')
         print(dataMath.rstrip(','))
         print(dataVerbal.rstrip(','))
-    return render_template('academics&GPA.html', mathScore = dataMath, verbalScore = dataVerbal, listOfStates = state)
-
+    return render_template('academics&GPA.html', mathScore = dataMath, verbalScore = dataVerbal)
+    
+def get_states():
+    with open('school_scores.json') as sat_data:
+        satData = json.load(sat_data)
+        listOfStates = []
+        option = ''
+        for states in satData:
+            if states['State']['Name'] not in listOfStates and states['Year'] == 2015:
+                listOfStates.append(states['State']['Name'])
+        for data in listOfStates:
+            option = option + Markup("<option value=\"" + states + "\">" + states + "</option>")
+    return option
+        
+        
 if __name__=="__main__":
     app.run(debug=False)
